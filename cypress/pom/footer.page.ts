f24c19eae78aec0f31524169a757c9496deee68b
leo ndi <ndileonel@gmail.com>
2024-02-12 15:22:29 +0000

    more footer fixes

--- cypress/pom/footer.page.ts
+++ cypress/pom/footer.page.ts
@@ -1,12 +1,12 @@
-import {checkLink} from "../support/common-helpers";
+import { checkLink } from "../support/common-helpers";
 
 class FooterPage {
     assertHrefs(accountType: string) {
-        if(accountType == "Submit" || accountType == "Search") {
+        if (accountType == "Submit" || accountType == "Search") {
             cy.get("footer").within(function () {
                 checkLink("Find a frame formulation", "/frame_formulations")
                 checkLink("How to notify nanomaterials", "/guidance/how-to-notify-nanomaterials")
-                checkLink("How to prepare images for notification",  "/guidance/how-to-prepare-images-for-notification")
+                checkLink("How to prepare images for notification", "/guidance/how-to-prepare-images-for-notification")
                 checkLink("Set up your authenticator app", "/guidance/how-to-set-up-authenticator-app")
                 checkLink("opss.enquiries@beis.gov.uk", "mailto:opss.enquiries@beis.gov.uk?subject=Help requested on Submit Cosmetic Product Notifications")
                 if (accountType == "Submit") {
@@ -20,15 +20,15 @@ class FooterPage {
         if (accountType == "OSU") {
             let loggedIn = false
             cy.get('header').within(function () {
-                cy.get("button").then($word => {
-                    if ($word.text() == "MenuSign out") {
-                        loggedIn = true
+                cy.get(".govuk-header__content").then(($content) => {
+                    if ($content.text().includes("Sign out")) {
+                        loggedIn = true;
                     }
-                })
-            }).then(function (){
+                });
+            }).then(function () {
                 if (loggedIn) {
                     cy.get("footer").within(function () {
-                        checkLink("Go to Cosmetic products search",'https://staging-search.cosmetic-product-notifications.service.gov.uk/support/sign-in')
+                        checkLink("Go to Cosmetic products search", 'https://staging-search.cosmetic-product-notifications.service.gov.uk/support/sign-in')
                         checkLink("Privacy policy", '/help/privacy-notice')
                         checkLink("Terms and conditions", '/help/terms-and-conditions')
                         checkLink("Cookies", '/help/cookies')
@@ -37,7 +37,7 @@ class FooterPage {
                     cy.get("footer").within(function () {
                         checkLink("Find a frame formulation", "/frame_formulations")
                         checkLink("How to notify nanomaterials", "/guidance/how-to-notify-nanomaterials")
-                        checkLink("How to prepare images for notification",  "/guidance/how-to-prepare-images-for-notification")
+                        checkLink("How to prepare images for notification", "/guidance/how-to-prepare-images-for-notification")
                         checkLink("Set up your authenticator app", "/guidance/how-to-set-up-authenticator-app")
                         checkLink("opss.enquiries@beis.gov.uk", "mailto:opss.enquiries@beis.gov.uk?subject=Help requested on Submit Cosmetic Product Notifications")
                         checkLink("Accessibility statement", "/help/accessibility-statement")
